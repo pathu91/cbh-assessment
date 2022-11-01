4da6290ac137a4880820fcb0a7c378840ddff794
Patrick <patrickhu91@gmail.com>
2022-11-01 14:48:16 -0700

    fix test

--- dpk.test.js
+++ dpk.test.js
@@ -22,14 +22,20 @@ describe("deterministicPartitionKey", () => {
   });
 
   it("Should hash partitionKeys with more than 256 characters", () => {
+    // const testEvent = {
+    //   partitionKey: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    //   Aenean turpis nunc, lobortis quis sagittis a, tempus molestie mauris.
+    //   Maecenas est nunc, maximus a lacus malesuada, eleifend porttitor dui.
+    //   In hac habitasse platea dictumst. Aliquam erat volutpat.
+    //   Cras sed vehicula nunc, eget consectetur tortor.`,
+    // };
     const testEvent = {
-      partitionKey: 1234567891234567891234567891234567891234567891234567891234561789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789,
+      partitionKey: 89173647182346891273491411234567891823746128934672389744912345678918237461289346723897412634789136498713468917364718234689127349141123456789182374612893467238974491234567891823746128934672389741263478913649871346891736471823468912734914112345678918237461289346723897449,
     };
     const trivialKey = deterministicPartitionKey(testEvent);
     const blob = new Blob([trivialKey]);
     const asBytes = blob.size;
-    // expect(testEvent.partitionKey.length > 256).toBe(true);
-    expect(String(testEvent.partitionKey).length < 256).toBe(true);
+    expect(Math.log(testEvent.partitionKey) >= 256).toBe(true);
     expect(asBytes).toBe(128);
   });
 });
